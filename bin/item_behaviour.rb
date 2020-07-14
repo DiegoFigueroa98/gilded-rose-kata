bc3c1f16b4a84883dc4de88606fe958f872fc4d3
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-07-14 16:43:57 -0500

    Add parent class ItemBehaviour
    Fix offenses detected by Rubocop.
    
    The parent class has all the necessary logic
    in any general item.

---
+++ bin/item_behaviour.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Parent class of items behaviour
+class ItemBehaviour
+  def set_quality(item, quality)
+    item.quality = quality
+    item.quality = 0 if quality <= 0
+    item.quality = 50 unless quality <= 50
+  end
+
+  def update_quality_pre_date_change(item)
+    set_quality(item, item.quality - 1)
+  end
+
+  def decrement_sell_date(item)
+    item.sell_in -= 1
+  end
+
+  def process_expired_item(item)
+    set_quality(item, item.quality - 1) if item.sell_in.negative?
+  end
+end
