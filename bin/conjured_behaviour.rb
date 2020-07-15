d11381abbfb81f175ad7f3209e95408aa5874be7
Diego Figueroa <16460315@colima.tecnm.mx>
2020-07-15 17:30:46 -0500

    Add subclass ConjuredBehaviour

---
+++ bin/conjured_behaviour.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Child class of ItemBehaviour
+class ConjuredBehaviour < ItemBehaviour
+  def update_quality_pre_date_change(item)
+    set_quality(item, item.quality - 2)
+  end
+
+  def process_expired_item(item)
+    set_quality(item, item.quality - 2) if item.sell_in.negative?
+  end
+end
