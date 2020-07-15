d92785fe0343a9e20f4531bc15499bcf8f3131c3
Andrea Silva <andiesilvamachado@gmail.com>
2020-07-15 17:39:23 -0500

    Add Subclass behaviour indentifier

---
+++ bin/behaviour_identifier.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require File.join(File.dirname(__FILE__), 'aged_brie_behaviour')
+require File.join(File.dirname(__FILE__), 'backstage_pass_behaviour')
+require File.join(File.dirname(__FILE__), 'sulfuras_behaviour')
+require File.join(File.dirname(__FILE__), 'conjured_behaviour')
+require File.join(File.dirname(__FILE__), 'item_behaviour')
+
+class BehaviourIdentifier
+  def obtain_behaviour(item)
+    return AgedBrieBehaviour.new if item.name == 'Aged Brie'
+    return SulfurasBehaviour.new if item.name == 'Sulfuras, Hand of Ragnaros'
+    return BackstagePassBehaviour.new if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    return ConjuredBehaviour.new if item.name == 'Conjured Mana Cake'
+    return ItemBehaviour.new
+  end
+end
